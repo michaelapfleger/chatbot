7d71174ecd396efa4b8c9c357400a33885b2057d
mpfleger <michaela.pfleger@elements.at>
2017-12-13 11:46:05 +0100

    Category Condition

--- BotController.php
+++ BotController.php
@@ -16,58 +16,101 @@ class BotController extends Website_Controller_Action
         $towns->setLimit(1);
         return $towns->load();
     }
+    private function getCategory($category) {
+        $categories = new Object_DemiCategory_List();
+        $categories->addConditionParam('name = ' . $category );
+        $categories->setLimit(1);
+        return $categories->load();
+    }
 
 
     private function startBooking($parameters) {
         $town = $this->getTown($parameters["demi_ort"]);
+        $category = $this->getCategory($parameters["demi_unterkunftsart"]);
         $list = new Object_DemiAccommodationServiceProvider_List();
         if ($town) {
             $list->addConditionParam('town__id = ' . $town[0]->getId());
         }
+        if ($category) {
+            $list->addConditionParam('categories LIKE "%,' . $category[0]->getId() . ',%"');
+        }
         $list->setOrderKey("name");
         $list->setOrder("ASC");
-        $list->setLimit(5);
-        $name = " --- ";
-        foreach($list->load() as $item) {
-            $name .= $item->getName() . " --- ";
-        }
-        return $name;
+        $list->setLimit(3);
+        return $list->load();
     }
 
-    private function printAcco($acco) {
-        $attachment = [
-            "color" => "#87B109",
-            "pretext" => "Hier habe ich einige Unterkünfte für dich zusammengestellt:",
-            "title" => $acco->getName(),
-            "text" => "Subline für Hotel 1",
-            "fields" => [
-                [
-                    "title" => "Kinderfreundlich",
-                    "value" => "High",
-                    "short" => false
-                ]
-            ],
-            "image_url" => "http://www.freeridecamps.at/wp-content/uploads/2016/10/Kleinwalsertal-01.png",
-            "thumb_url" => "http://www.freeridecamps.at/wp-content/uploads/2016/10/Kleinwalsertal-01.png",
-            "footer" => "Kleinwalsertal",
-            "footer_icon" => "http://www.freeridecamps.at/wp-content/uploads/2016/10/Kleinwalsertal-01.png",
-        ];
+    private function printAcco($accos) {
+        $attachments = array();
+        foreach ($accos as $acco) {
+            $image = $acco->getFirstImage(null, null);
+            if ($image) {
+                $thumbnail = $image->getThumbnail('demi_responsive_list');
+                $thumbnailUrl = "https://www.kleinwalsertal.com" . $thumbnail->getPath();
+            }
+            $address = $acco->getAddress();
+            $description = $acco->getDescription(null, 'de', null);
+            $website = $address->getUrl();
+            $mGroups = $acco->getMarketingGroups();
+            foreach($mGroups as $marketingGroup) {
+                if($marketingGroup->getFid() == "85392dfc-f8dc-4aa7-8c85-55b9ba91817b" || $marketingGroup->getFid() == "ca5d024e-d0d3-457e-8c21-7327fde12890"
+                    || $marketingGroup->getFid() == "58e7b7e0-2b4a-4d1c-b9ee-3b57d173dbe7") {
+                    $mg[$marketingGroup->getFid()] = $marketingGroup;
+                }
+            }
+            $attachment = [
+                "color" => "#87B109",
+//                "pretext" => "Hier habe ich einige Unterkünfte für dich zusammengestellt:",
+                "title" => $acco->getName(),
+                "text" => $acco->getCategoryNames(2),
+                "fields" => [
+                    [
+                        "title" => "Adresse",
+                        "value" => $address->getAddressLine1() . " " . $address->getAddressLine2() . "\n" . $address->getZipcode() . " " . $address->getTown() . " " . $address->getCity(),
+                        "short" => true
+                    ],
+                    [
+                        "title" => "Klassifizierung",
+                        "value" => Demi_Website_Helper::desklineStars($acco) ?: "keine",
+                        "short" => true
+                    ],
+                    [
+                        "title" => "Webste",
+                        "value" => $website ?: "-",
+                        "short" => true
+                    ],
+                    [
+                        "title" => $mg["85392dfc-f8dc-4aa7-8c85-55b9ba91817b"] ? "Bestpreisgarantie" : "",
+                        "value" => "",
+                        "short" => true
+                    ],
+                    [
+                        "title" => "Beschreibung",
+                        "value" => str_replace(["&auml;","&uuml;","&ouml;","&szlig;","&ndash;", "&bull;", "&nbsp;"],["ä","ü","ö","ß", "-", "•"," "],strip_tags($description)),
+                        "short" => false
+                    ],
+                ],
+                "image_url" => $thumbnailUrl,
+                "thumb_url" => "https://www.kleinwalsertal.com/static/img/sprite/mobile/best-price-badge.png",
+            ];
+            $attachments[] = $attachment;
+        }
+        return $attachments;
     }
 
     private function processMessage($update) {
         $parameters = $update["result"]["parameters"];
         if($update["result"]["action"] == "startbooking"){
-            $name = $this->startBooking($parameters);
+            $list = $this->startBooking($parameters);
+            $attachments = $this->printAcco($list);
             $this->sendMessage(array(
 //                "source" => $update["result"]["source"],
-                "speech" => "Hotel in " . $parameters["demi_ort"] . " für " . $parameters["amount_adults"] . " am " . $parameters["date"] . ": " . $name,
+//                "speech" => "Hotel in " . $parameters["demi_ort"] . " für " . $parameters["amount_adults"] . " am " . $parameters["date"] . ": " . $name,
                 "displayText" => "Hotel in " . $parameters["demi_ort"],
                 "data" => [
                     "slack" => [
-                        "attachments" => [
-                            [
-                            ]
-                        ]
+                        "text" => "*Hier habe ich einige Unterkünfte für dich zusammengestellt:*",
+                        "attachments" => $attachments,
                     ]
                 ]
 //                "contextOut" => array()
@@ -80,6 +123,17 @@ class BotController extends Website_Controller_Action
     }
 
     public function webhookAction() {
+//        $list = new Object_DemiAccommodationServiceProvider_List();
+//        $list->addConditionParam('categories LIKE "%,' . "90507" . ',%"');
+//        $list->setOrderKey("name");
+//        $list->setOrder("ASC");
+//        $list->setLimit(1);
+//
+//        foreach ($list as $acco) {
+//            p_r($acco->getId());
+//        }
+//
+//        die();
         $this->getResponse()->setHeader("X-Robots-Tag", "noindex, nofollow", true);
 //        $this->disableLayout();
 
